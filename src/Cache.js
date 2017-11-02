18aebb3e618e8df8a2f4005165fefc35e304df82
Hitesh Khandelwal <hitesh@smartprix.com>
2017-11-02 20:37:49 +0530

    improve Cache library and add tests for it

--- src/Cache.js
+++ src/Cache.js
@@ -1,133 +1,172 @@
 /* eslint-disable guard-for-in */
 import EventEmitter from 'events';
 
+const FETCHING = Symbol('Fetching_Value');
 let globalCache;
 
-class TTL {
+class Cache {
 	constructor() {
 		this.data = {};
+		this.ttl = {};
+		this.events = new EventEmitter();
 	}
 
-	set(key, ttl, callback) {
-		clearTimeout(this.data[key]);
-		this.data[key] = setTimeout(callback, ttl);
-	}
-
-	del(key) {
-		clearTimeout(this.data[key]);
-		delete this.data[key];
-	}
-
-	clear() {
-		for (const ttl in this.data) {
-			clearTimeout(this.data[ttl]);
-		}
-		this.data = {};
-	}
-}
-
-class Store {
-	constructor() {
-		this.data = {};
-	}
-
-	async get(key, defaultValue = undefined) {
-		if (key in this.data) return this.data[key];
-		return defaultValue;
-	}
-
-	async set(key, value, ttl = 0) {
+	_set(key, value, ttl = 0) {
 		if (ttl <= 0) {
 			this.data[key] = value;
 			return;
 		}
 
-		this.set(key, value);
+		// set value
+		this.data[key] = value;
 
-		if (!this.ttl) this.ttl = new TTL();
-		this.ttl.set(key, ttl, () => this.del(key));
+		// set ttl
+		clearTimeout(this.ttl[key]);
+		this.ttl[key] = setTimeout(() => this.del(key), ttl);
 	}
 
-	async del(key) {
-		if (this.ttl) this.ttl.del(key);
-		delete this.cache[key];
+	_del(key) {
+		// delete ttl
+		if (key in this.ttl) {
+			clearTimeout(this.ttl[key]);
+			delete this.ttl[key];
+		}
+
+		// delete data
+		delete this.data[key];
 	}
 
+	_clear() {
+		// clear ttl
+		for (const key in this.ttl) {
+			clearTimeout(this.ttl[key]);
+		}
+		this.ttl = {};
+
+		// clear data
+		this.data = {};
+	}
+
+	/**
+	 * gets a value from the cache
+	 * @param {string} key
+	 * @param {any} defaultValue
+	 */
+	async get(key, defaultValue = undefined) {
+		const existing = this.data[key];
+		if (existing === FETCHING) {
+			// Some other process is still fetching the value
+			// Don't dogpile shit, wait for the other process
+			// to finish it
+			return new Promise((resolve) => {
+				this.events.once(`get:${key}`, resolve);
+			});
+		}
+
+		if (existing === undefined) return defaultValue;
+		return existing;
+	}
+
+	/**
+	 * checks if a key exists in the cache
+	 * @param {string} key
+	 */
 	async has(key) {
 		return (key in this.data);
 	}
 
-	async clear() {
-		if (this.ttl) this.ttl.clear();
-		this.data = {};
+	/**
+	 * sets a value in the cache
+	 * avoids dogpiling if the value is a promise or a function returning a promise
+	 * @param {string} key
+	 * @param {any} value
+	 * @param {int|object} options either ttl in ms, or object of {ttl}
+	 */
+	async set(key, value, options = {}) {
+		let ttl;
+		if (typeof options === 'number') {
+			ttl = options;
+		}
+		else {
+			ttl = options.ttl || 0;
+		}
+
+		this.data[key] = FETCHING;
+
+		try {
+			if (value && value.then) {
+				// value is a Promise
+				// resolve it and then cache it
+				const resolvedValue = await value;
+				this._set(key, value, ttl);
+				this.events.emit(`get:${key}`, resolvedValue);
+				return true;
+			}
+			else if (typeof value === 'function') {
+				// value is a function
+				// call it and set the result
+				return this.set(key, value(key), ttl);
+			}
+
+			// value is normal
+			// just set it in the store
+			this._set(key, value, ttl);
+			this.events.emit(`get:${key}`, value);
+			return true;
+		}
+		catch (error) {
+			this._del(key);
+			this.events.emit(`get:${key}`, undefined);
+			return false;
+		}
 	}
 
-	async size() {
-		return Object.keys(this.cache).length;
-	}
-}
-
-class Cache {
-	constructor() {
-		this.fetching = {};
-		this.store = new Store();
-	}
-
-	get(key, defaultValue = undefined) {
-		if (this.fetching[key]) {
+	/**
+	 * gets a value from the cache, or sets it if it doesn't exist
+	 * this takes care of dogpiling (make sure value is a function to avoid dogpiling)
+	 * @param {string} key key to get
+	 * @param {any} value value to set if the key does not exist
+	 * @param {int|object} options either ttl in ms, or object of {ttl}
+	 */
+	async getOrSet(key, value, options = {}) {
+		const existing = this.data[key];
+		if (existing === FETCHING) {
 			// Some other process is still fetching the value
 			// Don't dogpile shit, wait for the other process
 			// to finish it
-			if (!this.events) Promise.reject(new Error('Cache EventEmitter Missing'));
-
-			return new Promise((resolve, reject) => {
-				this.events.once(`get:${key}`, ({error, value}) => {
-					if (error) reject(error);
-					else resolve(value);
-				});
+			return new Promise((resolve) => {
+				this.events.once(`get:${key}`, resolve);
 			});
 		}
 
-		return this.store.get(key, defaultValue);
+		// key already exists, return it
+		if (existing !== undefined) return existing;
+
+		this.data[key] = FETCHING;
+		await this.set(key, value, options);
+		return this.data[key];
 	}
 
-	async set(key, value, ttl = 0) {
-		if (value && value.then) {
-			// value is a Promise
-			// resolve it and then cache it
-			if (!this.events) this.events = new EventEmitter();
-			this.fetching[key] = true;
-			try {
-				const resolvedValue = await value;
-				await this.store.set(key, resolvedValue, ttl);
-
-				delete this.fetching[key];
-				this.events.emit(`get:${key}`, {value: resolvedValue});
-
-				return true;
-			}
-			catch (error) {
-				// Ignore Error
-				delete this.fetching[key];
-				this.events.emit(`get:${key}`, {error});
-				return false;
-			}
-		}
-
-		await this.store.set(key, value, ttl);
-		return true;
-	}
-
+	/**
+	 * deletes a value from the cache
+	 * @param {string} key
+	 */
 	async del(key) {
-		return this.store.del(key);
+		return this._del(key);
 	}
 
+	/**
+	 * returns the size of the cache (no. of keys)
+	 */
 	async size() {
-		return this.store.size();
+		return Object.keys(this.data).length;
 	}
 
+	/**
+	 * clears the cache (deletes all keys)
+	 */
 	async clear() {
-		return this.store.clear();
+		return this._clear();
 	}
 
 	static globalCache() {
@@ -139,8 +178,16 @@ class Cache {
 		return this.globalCache().get(key);
 	}
 
-	static set(key, value, ttl) {
-		return this.globalCache().set(key, value, ttl);
+	static has(key) {
+		return this.globalCache().has(key);
+	}
+
+	static set(key, value, options = {}) {
+		return this.globalCache().set(key, value, options);
+	}
+
+	static getOrSet(key, value, options = {}) {
+		return this.globalCache().getOrSet(key, value, options);
 	}
 
 	static del(key) {
---
+++ test/cache.test.js
@@ -0,0 +1,132 @@
+import {expect} from 'chai';
+import {Cache} from '../src/index';
+
+const cache = new Cache();
+function sleep(val, timeout = 20) {
+	return new Promise(resolve => setTimeout(() => resolve(val), timeout));
+}
+
+describe('cache library', () => {
+	it('should get and set values', async () => {
+		const key = 'a';
+		const value = 'this';
+		expect(await cache.get(key)).to.be.undefined;
+		expect(await cache.set(key, value)).to.be.true;
+		expect(await cache.get(key)).to.equal(value);
+	});
+
+	it('should correctly resolve promises', async () => {
+		const key = 'b';
+		const val = 'is';
+		const value = sleep(val);
+
+		expect(await cache.get(key)).to.be.undefined;
+		const setPromise = cache.set(key, value);
+		const getPromise1 = cache.get(key);
+		const getPromise2 = cache.get(key);
+		const getPromise3 = cache.get(key);
+
+		expect(await cache.get(key)).to.equal(val);
+		expect(await setPromise).to.be.true;
+		expect(await getPromise1).to.equal(val);
+		expect(await getPromise2).to.equal(val);
+		expect(await getPromise3).to.equal(val);
+	});
+
+	it('should correctly resolve functions', async () => {
+		const key = 'c';
+		const val = 'really';
+		const value = () => val;
+		expect(await cache.get(key)).to.be.undefined;
+		const promise = cache.set(key, value);
+		expect(await cache.get(key)).to.equal(val);
+		expect(await promise).to.be.true;
+		expect(await cache.get(key)).to.equal(val);
+	});
+
+	it('should correctly resolve functions with promises', async () => {
+		const key = 'd';
+		const val = 'awesome';
+		let counter = 0;
+		const value = () => {
+			counter++;
+			return sleep(val);
+		};
+
+		expect(await cache.get(key)).to.be.undefined;
+		const setPromise = cache.set(key, value);
+		const getPromise1 = cache.get(key);
+		const getPromise2 = cache.get(key);
+		const getPromise3 = cache.get(key);
+		expect(await cache.get(key)).to.equal(val);
+		expect(await setPromise).to.be.true;
+		expect(await getPromise1).to.equal(val);
+		expect(await getPromise2).to.equal(val);
+		expect(await getPromise3).to.equal(val);
+		expect(counter).to.equal(1);
+	});
+
+	it('should correctly getOrSet', async () => {
+		let key = 'e';
+		let val = 'man';
+		let value = val;
+		expect(await cache.getOrSet(key, value)).to.equal(val);
+		expect(await cache.getOrSet(key, 'anything')).to.equal(val);
+
+		key = 'f';
+		val = 'do';
+		let counter = 0;
+		value = () => {
+			counter++;
+			return sleep(val);
+		};
+
+		cache.getOrSet(key, value);
+		cache.getOrSet(key, value);
+		cache.getOrSet(key, value);
+		expect(await cache.getOrSet(key, value)).to.equal(val);
+		expect(await cache.getOrSet(key, value)).to.equal(val);
+		expect(await cache.getOrSet(key, value)).to.equal(val);
+		expect(counter).to.equal(1);
+	});
+
+	it('should correctly set ttl', async () => {
+		const key = 'g';
+		const value = 'you';
+		expect(await cache.get(key)).to.be.undefined;
+		expect(await cache.set(key, value, 20)).to.be.true;
+		expect(await cache.get(key)).to.equal(value);
+		await sleep('', 10);
+		expect(await cache.get(key)).to.equal(value);
+		await sleep('', 7);
+		expect(await cache.get(key)).to.equal(value);
+		await sleep('', 4);
+		expect(await cache.get(key)).to.be.undefined;
+	});
+
+	it('should correctly delete', async () => {
+		expect(await cache.get('e')).to.equal('man');
+		await cache.del('e');
+		expect(await cache.get('e')).to.be.undefined;
+	});
+
+	it('should correctly check for existance', async () => {
+		expect(await cache.has('d')).to.be.true;
+		expect(await cache.has('d1')).to.be.false;
+	});
+
+	it('should correctly return the size', async () => {
+		const cache1 = new Cache();
+		expect(await cache1.size()).to.equal(0);
+		expect(await cache.size()).to.equal(5);
+	});
+
+	it('should correctly clear the cache', async () => {
+		expect(await cache.get('a')).to.equal('this');
+		await cache.clear();
+		expect(await cache.get('a')).to.be.undefined;
+		expect(await cache.get('b')).to.be.undefined;
+		expect(await cache.size()).to.equal(0);
+	});
+});
+
