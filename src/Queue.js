31c2b3bc61f3e292b7ed838a51107dedcb839437
Rohit Gohri <rohit.gohri@smartprix.com>
2018-02-01 12:34:50 +0530

    Update comments and make code uniform

--- src/Queue.js
+++ src/Queue.js
@@ -5,6 +5,11 @@ let processorWrapper;
 class Queue {
 	static jobs;
 
+	/**
+	 * Class constructor : Create a new Queue
+	 * @param {String} name Name of the queue
+	 * @param {Object} [redis={port: 6379, host: '127.0.0.1'}] Redist connection settings object
+	 */
 	constructor(name, redis = {port: 6379, host: '127.0.0.1'}) {
 		this.name = name;
 
@@ -24,7 +29,7 @@ class Queue {
 
 	/**
 	 * Add a job to the Queue
-	 * @param {Object} jobData Job data
+	 * @param {*} jobData Job data
 	 * @param {Number|String} priority Priority of the job
 	 * @returns {Number} The ID of the job created
 	 */
@@ -59,7 +64,7 @@ class Queue {
 
 	/**
 	 * Set number of retry attempts for any job added after this is called
-	 * @param {Number} attempts Number of attempts (>= 0)
+	 * @param {Number} attempts Number of attempts (>= 0), default = 1
 	 */
 	setAttempts(attempts) {
 		this.attempts = attempts;
@@ -67,7 +72,7 @@ class Queue {
 
 	/**
 	 * Set delay b/w successive jobs
-	 * @param {Number} delay Delay b/w jobs, milliseconds
+	 * @param {Number} delay Delay b/w jobs, milliseconds, default = 0
 	 */
 	setDelay(delay) {
 		this.delay = delay;
@@ -75,32 +80,40 @@ class Queue {
 
 	/**
 	 * Sets removeOnComplete for any job added to this Queue from now on
-	 * @param {Boolean} removeOnComplete True/False
+	 * @param {Boolean} removeOnComplete default = false
 	 */
 	setRemoveOnCompletion(removeOnComplete) {
 		this.removeOnComplete = removeOnComplete;
 	}
 
 	/**
-	 * Sets noFailure for any job added to this Queue from now on
-	 * This will mark the job the complete even if it fails
-	 * @param {Boolean} noFailure True/False
+	 * Sets noFailure for any job added to this Queue from now on.
+	 * This will mark the job complete even if it fails when true
+	 * @param {Boolean} noFailure default = false
 	 */
 	setNoFailure(noFailure) {
 		this.noFailure = noFailure;
 	}
 
 	/**
-	 * Processor function : async
-	 * @param {Object} jobData The information saved in the job during adding
-	 * @param {Object} ctx Optional - Can be used to pause and resume queue
+	 * An async function which will be called to process the job data
+	 * @callback Queue~processorCallback
+	 * @param {*} jobData The information saved in the job during adding of job
+	 * @param {Object} [ctx] Can be used to pause and resume queue,
+	 * 	Will only be passed when attaching a processor to the queue
+	 * 		Usage:
+	 * 		ctx.pause(timeout, callback()) :
+	 * 			Waits for any active jobs to complete till timeout,
+	 * 			then forcefully shuts them down (like shutdown)
+	 * 		ctx.resume() : Resumes Queue processing
+	 * 		For detailed info : https://github.com/Automattic/kue#pause-processing
+	 * @returns {*} Must return something, will be saved/returned
 	 */
 
 	/**
-	 * Attach a processor to the Queue which will keep getting jobs as it does them
-	 * @param {Function} processor An async function which will be called to process the job data
-	 * @param {Number} concurrency The number of jobs this processor
-	 * 		can handle concurrently/parallely
+	 * Attach a processor to the Queue which will keep getting jobs as it completes them
+	 * @param {Queue~processorCallback} processor
+	 * @param {Number} [concurrency=1] The number of jobs this processor can handle parallely
 	 */
 	addProcessor(processor, concurrency = 1) {
 		Queue.jobs.process(this.name, concurrency, async (job, ctx, done) => {
@@ -122,11 +135,17 @@ class Queue {
 	}
 
 	/**
+	 * Job processing result
+	 * @typedef {Object} jobResult
+	 * @property {*} res Result returned by processor function
+	 * @property {jobDetails} job Job status object
+	 */
+
+	/**
 	 * Process a single job in the Queue and mark it complete or failed,
 	 * for when you want to manually process jobs
-	 * @param {Function} processor An async function which will be called to process the job data
-	 * @returns {Object} Result of processor function and
-	 * 		Job object of completed job (same as returned by status)
+	 * @param {Queue~processorCallback} processor Called without ctx
+	 * @returns {jobResult} Result of processor function and job object of completed job
 	 */
 	async processJob(processor) {
 		return new Promise((resolve, reject) => {
@@ -141,9 +160,26 @@ class Queue {
 	}
 
 	/**
+	 * Job status object.
+	 * Only listed important properties, there maybe more
+	 * @typedef {Object} jobDetails
+	 * @property {Number} id
+	 * @property {String} type Name of the Queue
+	 * @property {Object} data : {jobData, options}
+	 * @property {*} result Result of the processor callback
+	 * @property {Number} priority
+	 * @property {String} state One of {'inactive', 'delayed' ,'active', 'complete', 'failed'}
+	 * @property {*} error
+	 * @property {Number} created_at unix time stamp
+	 * @property {Number} delay delay in milliseconds, if any was set
+	 * @property {Number} ttl TTL in milliseconds, if any was set
+	 * @property {Object} attempts Attempts Object
+	 */
+
+	/**
 	 * Function to query the status of a job
 	 * @param {Number} jobId Job id for which status info is required
-	 * @returns {Object} Object full of job details like state, time, attempts, etc.
+	 * @returns {jobDetails} Object full of job details like state, time, attempts, etc.
 	 */
 	static async status(jobId) {
 		return new Promise((resolve, reject) => {
@@ -158,9 +194,8 @@ class Queue {
 	/**
 	 * Manualy process a specific Job
 	 * @param {Number} jobId Id of the job to be processed
-	 * @param {Function} processor The function which will be called with the job data
-	 * @returns {Object} Result of processor function and
-	 * 		Job object of completed job (same as returned by status)
+	 * @param {Queue~processorCallback} processor Called without ctx
+	 * @returns {jobResult} Result of processor function and job object of completed job
 	 */
 	static async processJobById(jobId, processor) {
 		return new Promise((resolve, reject) => {
@@ -173,13 +208,16 @@ class Queue {
 
 	/**
 	 * Function shuts down the Queue gracefully.
-	 * Waits for active jobs to complete until timeout,
-	 * then marks them failed.
-	 * @param {Number} timeout Time in milliseconds, default = 5000
+	 * Waits for active jobs to complete until timeout, then marks them failed.
+	 * @param {Number} [timeout=5000] Time in milliseconds, default = 5000
+	 * @returns {Boolean}
 	 */
 	static async exit(timeout = 5000) {
-		return new Promise((resolve, reject) => {
-			if (Queue.jobs === undefined) reject(new Error('Queue not initialized'));
+		return new Promise((resolve) => {
+			if (Queue.jobs === undefined) {
+				resolve(true);
+				return;
+			}
 			Queue.jobs.shutdown(timeout, (err) => {
 				console.log('Sm-utils Queue shutdown: ', err || '');
 				resolve(true);
@@ -191,23 +229,25 @@ class Queue {
 	 * Cleanup function to be called during startup,
 	 * resets active jobs older than specified time
 	 * @param {String} name Queue name
-	 * @param {Number} olderThan Time in milliseconds, default = 5000
-	 * @returns {Boolean} Cleanup Done or not
+	 * @param {Number} [olderThan=5000] Time in milliseconds, default = 5000
 	 */
 	static async cleanup(name, olderThan = 5000) {
-		if (Queue.jobs === undefined) return false;
+		if (Queue.jobs === undefined) throw new Error('Queue not initialized');
 		const n = await new Promise((resolve, reject) => Queue.jobs.activeCount(name, (err, total) => {
-			if (err) reject(new Error('Could not get total active jobs'));
-			resolve(total);
+			if (err) reject(new Error('Could not get total active jobs: ' + err));
+			else resolve(total);
 		}));
 		return new Promise((resolve, reject) => {
 			kue.job.rangeByType(name, 'active', 0, n, 'asc', (err, jobs) => {
-				if (err) reject(new Error('Could not fetch jobs: ' + err));
+				if (err) {
+					reject(new Error('Could not fetch jobs: ' + err));
+					return;
+				}
 				for (let i = 0; i < jobs.length; i++) {
 					if (Date.now() - jobs[i].created_at > olderThan) { jobs[i].inactive() }
 					else break;
 				}
-				resolve(true);
+				resolve();
 			});
 		});
 	}
