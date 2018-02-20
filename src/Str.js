79c9ca3212f96d27fcf4ed09d6faddd421329112
Hitesh Khandelwal <hitesh@smartprix.com>
2018-02-20 15:06:13 +0530

    add transform function to str library

--- src/Str.js
+++ src/Str.js
@@ -62,10 +62,37 @@ function pluralize(str, count = 2) {
 	return plural(str);
 }
 
+/**
+ * transform a string by replacing characters from from string to to string
+ * eg. `Str.transform('abc', 'bc', 'de') // returns ade`
+ * @param {string} str string to transform
+ * @param {string} from characters to replace in the string
+ * @param {string} to characters to replace with in the string
+ * @returns {string} transformed string
+ */
+function transform(str, from, to) {
+	const len = str.length;
+	let out = '';
+	let pos;
+
+	for (let i = 0; i < len; i++) {
+		pos = from.indexOf(str.charAt(i));
+		if (pos >= 0) {
+			out += to.charAt(pos);
+		}
+		else {
+			out += str.charAt(i);
+		}
+	}
+
+	return out;
+}
+
 export default {
 	invertCase,
 	isVowel,
 	isConsonant,
 	plural,
 	pluralize,
+	transform,
 };
--- test/str.test.js
+++ test/str.test.js
@@ -9,4 +9,8 @@ describe('str library', () => {
 	it('should correctly plural words', () => {
 		expect(Str.plural('smartphone')).to.equal('smartphones');
 	});
+
+	it('should correctly transform string', () => {
+		expect(Str.transform('abc', 'bc', 'de')).to.equal('ade');
+	});
 });
