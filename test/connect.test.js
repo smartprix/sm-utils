794d682e888e2d38a4b91d277f449ebcfe90228d
Hitesh Khandelwal <hitesh@smartprix.com>
2018-05-16 15:43:34 +0530

    modify connect test

--- test/connect.test.js
+++ test/connect.test.js
@@ -2,43 +2,41 @@ import fs from 'fs';
 import {expect} from 'chai';
 import {Connect, File} from '../src/index';
 
-const responsePath = `${__dirname}/response.html`;
-
-async function makeApiCall() {
-	const connect = Connect.url('https://www.lipsum.com/feed/html');
-	connect.userAgent('mobile');
-	const options = {
-		amount: 1,
-		what: 'paras',
-		start: 'yes',
-		generate: 'Generate Lorem Ipsum',
-	};
-	connect.fields(options);
-	connect.save(responsePath);
-
-	return connect.get();
-}
-
 describe('connect class', () => {
-	let response;
 	before(async function () {
 		this.timeout(10000);
-		response = await makeApiCall();
 	});
 
-	describe('test connect class functions', async () => {
-		it('should correctly fetch the response', () => {
-			expect(response.statusCode).to.equal(200);
-			expect(response.body).to.match(/Lorem ipsum dolor sit amet/);
-		});
+	it('should correctly fetch the response', async () => {
+		const response = await Connect.url('https://www.lipsum.com/feed/html')
+			.userAgent('mobile')
+			.fields({
+				amount: 1,
+				what: 'paras',
+				start: 'yes',
+				generate: 'Generate Lorem Ipsum',
+			});
 
-		it('should correctly save the response', async () => {
-			const body = await (new File(responsePath)).read();
-			expect(body).to.equal(response.body.toString());
-		});
+		expect(response.statusCode).to.equal(200);
+		expect(response.body).to.match(/Lorem ipsum dolor sit amet/);
 	});
 
-	after(async () => {
+	it('should correctly save the response', async () => {
+		const responsePath = `${__dirname}/response.html`;
+		const response = await Connect.url('https://www.lipsum.com/feed/html')
+			.userAgent('mobile')
+			.fields({
+				amount: 1,
+				what: 'paras',
+				start: 'yes',
+				generate: 'Generate Lorem Ipsum',
+			})
+			.save(responsePath);
+
+		const body = await File(responsePath).read();
+		expect(body).to.match(/Lorem ipsum dolor sit amet/);
+		expect(body).to.equal(response.body.toString());
+
 		await fs.unlink(responsePath, (err) => {
 			if (err) throw err;
 		});
