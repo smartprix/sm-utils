194e06adac606b3e9f73872b3cdb4f15597b6678
Rohit Gohri <rohit.gohri@smartprix.com>
2018-09-04 17:22:11 +0530

    move d to it's own folder, along with it's type file

---
+++ d/index.js
@@ -0,0 +1,4 @@
+const d = require('../dist/debug/d');
+
+global.d = d;
+module.exports = d;
--- index.d.ts
+++ index.d.ts
@@ -2190,30 +2190,3 @@ declare module 'sm-utils' {
 
     const baseConvert: typeof Crypt.baseConvert;
 }
-
-
-declare module 'sm-utils/d' {
-    interface uncaughtHandlerOpts {
-        /**
-         * default: true
-         */
-        exceptions?: boolean
-        /**
-         * default: true
-         */
-        rejections?: boolean
-    }
-
-    /**
-     * Colored Log to console with stack trace
-     * @param args Args to log to console
-     */
-    function d(...args: any[]): void;
-
-    function trace(error?: string | Error) : void;
-    function getTrace(error?: Error) : any;
-    function dump(...args: any[]): void;
-    function enableUncaughtHandler(options?: uncaughtHandlerOpts): void;
-    function disableUncaughtHandler(options?: uncaughtHandlerOpts): void;
-
-}
