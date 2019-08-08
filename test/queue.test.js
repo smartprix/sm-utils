7eca4c581597118d74ae52360a52758cbd28c9c5
rishabh-smpx <rishabh.singh@smartprix.com>
2019-08-08 17:49:46 +0530

    Fix default value

--- test/queue.test.js
+++ test/queue.test.js
@@ -11,7 +11,7 @@ function sleep(val, timeout = 20) {
 const processor = jobData => jobData.data;
 
 before(async () => {
-	queue = new Queue('test', {host: process.env.DRONE ? 'redis' : undefined});
+	queue = new Queue('test', {host: process.env.DRONE ? 'redis' : '127.0.0.1'});
 	await queue.delete(0);
 });
 
